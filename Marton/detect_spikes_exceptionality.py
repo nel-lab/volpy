73a38413fe93978f87ed34b892b8022e0725cc3f
agiovann <53313b8438a03d23251c212228260ee7c0d99a41>
2020-05-13 09:27:17 -0400

    good results with simple algorithm

--- Marton/detect_spikes_exceptionality.py
+++ Marton/detect_spikes_exceptionality.py
@@ -147,16 +147,202 @@ def metric(sweep_time, e_sg, e_sp, e_t, e_sub, v_sg, v_sp, v_t, v_sub, save=Fals
 
     return precision, recall, F1, sub_corr, e_match, v_match, mean_time
 
-
 #%%
-file_list = ['/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data/454597_Cell_0_40x_patch1_output_simple.npz']
-file_list = ['/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data/454597_Cell_0_40x_patch1_output_simple.npz', 
-             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data/456462_Cell_3_40x_1xtube_10A2_output_simple.npz',
-             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data/456462_Cell_3_40x_1xtube_10A3_output_simple.npz',
-             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data/456462_Cell_5_40x_1xtube_10A5_output_simple.npz',
-             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data/456462_Cell_5_40x_1xtube_10A7_output_simple.npz',
-             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data/462149_Cell_1_40x_1xtube_10A1_output_simple.npz', 
-             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data/462149_Cell_1_40x_1xtube_10A2_output_simple.npz']
+from caiman.base.movies import rolling_window
+from functools import partial
+import cv2
+import scipy
+from scipy.interpolate import interp1d
+import scipy
+
+def estimate_running_std(signal_in, win_size=20000, stride=5000, 
+                         idx_exclude=None, q_min=25, q_max=75):
+    """
+    Function to estimate ROBUST runnning std
+    
+    Args:
+        win_size: int
+            window used to compute running std to normalize signals when 
+            compensating for photobleaching
+            
+        stride: int
+            corresponding stride to win_size
+            
+        idx_exclude: iterator
+            indexes to exclude when computing std
+        
+        q_min: float
+            lower percentile for estimation of signal variability (do not change)
+        
+        q_max: float
+            higher percentile for estimation of signal variability (do not change)
+        
+        
+    Returns:
+        std_run: ndarray
+            running standard deviation
+    
+    """
+    if idx_exclude is not None:
+        signal = signal_in[np.setdiff1d(range(len(signal_in)), idx_exclude)]        
+    else:
+        signal = signal_in
+    iter_win = rolling_window(signal[None,:],win_size,stride)
+    myperc = partial(np.percentile, q=[q_min,q_max], axis=-1)
+    res = np.array(list(map(myperc,iter_win))).T.squeeze()
+    iqr = (res[1]-res[0])/1.35
+    std_run = cv2.resize(iqr,signal_in[None,:].shape).squeeze()
+    return std_run
+
+def extract_exceptional_events(z_signal, thres_STD=5, N=2, min_dist=1, bidirectional=False):
+    """
+    Extract peaks that are stastically significant over estimated noise
+    
+    
+    N: int
+        window used to compute exceptionality (higher frame rates than 1Khz
+        MIGHT need larger N) 
+    
+    thres_STD: float
+            threshold related to z scored signal 
+        
+    min_dist: int
+        min distance between peaks
+        
+    bidirectional: bool
+            whether to build an error function that accounts for the direction
+            of signal (it does not seem to help using this)
+            
+    Returns:
+        indexes: list
+            indexes of inferred spikes 
+
+        erf: ndarray
+            float representing the exceptionality of the trace over N points
+    
+    """
+    if bidirectional:        
+        erf = scipy.special.log_ndtr(-np.abs(z_signal))
+    else:
+        erf = scipy.special.log_ndtr(-z_signal)
+    erf = np.cumsum(erf)
+    erf[N:] -= erf[:-N]
+    indexes = peakutils.indexes(-erf, thres=thres_STD, min_dist=min_dist, thres_abs=True)
+    return indexes, erf
+    
+#%%     
+def find_spikes(signal, signal_no_subthr=None, thres_STD=5, thres_STD_ampl=4, 
+                min_dist=1, N=2, win_size=20000, stride=5000, spike_before=3, 
+                spike_after=4, q_min=25, q_max=75, bidirectional=False):
+    """
+    Function that extracts spike from np.diff(signal). In general the only 
+    parameters that should be adapted are thres_STD and 
+    @TODO: extract subthreshold signal as well
+    
+    Args:
+        signal: ndarray
+            fluorescence signal after detrending
+        
+        signal_no_subthr: ndarray
+            signal without subthreshold activity 
+            
+        thres_STD: float
+            threshold related to z scored signal 
+            
+        thres_STD_ampl: float
+            threshold related to z scored signal without subthreshold activity
+            
+        min_dist: int
+            min distance between spikes
+            
+        N: int
+            window used to compute exceptionality (higher frame rates than 1Khz
+            MIGHT need larger N) 
+            
+        win_size: int
+            window used to compute running std to normalize signals when 
+            compensating for photobleaching
+            
+        stride: int
+            corresponding stride to win_size
+            
+        spike_before: int
+            how many sample to remove before a spike when removing the spike
+            from the trace
+        
+        spike_after: int
+            how many sample to remove after a spike when removing the spike
+            from the trace        
+            
+        q_min: float
+            lower percentile for estimation of signal variability (do not change)
+        
+        q_max: float
+            higher percentile for estimation of signal variability (do not change)
+            
+        bidirectional: bool
+            whether to build an error function that accounts for the direction
+            of signal (it does not seem to help using this)
+            
+        
+    Returns:
+        indexes: list
+            indexes of inferred spikes 
+
+        erf: ndarray
+            float representing the exceptionality of the trace over N points
+        
+        z_signal: ndarray
+            z scored signal
+        
+    """
+    
+    signal = np.diff(signal)
+    std_run = estimate_running_std(signal, win_size, stride, q_min=q_min, q_max=q_max)
+    z_signal = signal/std_run
+    index_exceptional,_ = extract_exceptional_events(z_signal, thres_STD=thres_STD, N=N, min_dist=min_dist, bidirectional=bidirectional)    
+    index_remove = np.concatenate([index_exceptional+ii for ii in range(-spike_before,spike_after)])
+    std_run = estimate_running_std(signal, win_size, stride,idx_exclude=index_remove, q_min=q_min, q_max=q_max)
+    z_signal = signal/std_run 
+    indexes, erf = extract_exceptional_events(z_signal, thres_STD=thres_STD, N=N, min_dist=min_dist, bidirectional=bidirectional)
+    # remove spikes that are not large peaks in the original signal
+    if signal_no_subthr is not None:
+        signal_no_subthr /= estimate_running_std(signal_no_subthr, 20000, 5000, 
+                                                 q_min=q_min, q_max=q_max)
+        
+        indexes = np.intersect1d(indexes,np.where(signal_no_subthr[1:]>thres_STD_ampl))
+        
+    return indexes, erf, z_signal
+#%%
+def nan_helper(y):
+    return np.isnan(y), lambda z: z.nonzero()[0]
+
+
+def estimate_subthreshold(signal, thres_STD = 5, spike_before=3, spike_after=4, kernel_size=21):
+  delta_sig = np.diff(signal)
+  index_exceptional, erf, z_sig = find_spikes(delta_sig, thres_STD=thres_STD)
+  index_remove = np.concatenate([index_exceptional+ii for ii in range(-spike_before,spike_after)])
+  sig_sub = signal.copy()
+  sig_sub[np.minimum(index_remove,len(signal)-1)] = np.nan
+  nans, x= nan_helper(sig_sub)
+  sig_sub[nans]= np.interp(x(nans), x(~nans), sig_sub[~nans])
+  sig_sub = scipy.signal.medfilt(sig_sub, kernel_size=kernel_size)
+  return sig_sub
+
+def normalize(ss):
+    aa = (ss-np.min(ss))/(np.max(ss)-np.min(ss))
+    aa -= np.median(aa)
+    aa /= estimate_running_std(aa)
+    return aa
+#%%
+file_list = ['/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data_new/454597_Cell_0_40x_patch1_output.npz', 
+             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data_new/456462_Cell_3_40x_1xtube_10A2_output.npz',
+             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data_new/456462_Cell_3_40x_1xtube_10A3_output.npz',
+             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data_new/456462_Cell_5_40x_1xtube_10A5_output.npz',
+             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data_new/456462_Cell_5_40x_1xtube_10A7_output.npz',
+             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data_new/462149_Cell_1_40x_1xtube_10A1_output.npz', 
+             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data_new/462149_Cell_1_40x_1xtube_10A2_output.npz',
+             '/Users/agiovann/NEL-LAB Dropbox/NEL/Papers/VolPy/Marton/data_new/456462_Cell_5_40x_1xtube_10A6_output.npz']
 
 fig = plt.figure(figsize=(12,12))
 temp = file_list[0].split('/')[-1].split('_')
@@ -166,12 +352,19 @@ pr= []
 re = []
 F = []
 sub = []
-N_opt = [2,2,2,2, 2,2]
-Thr_opt = [8.5,8.5, 5.5, 10.5, 6.5,10.5, 6.5 ]
-F1 = [0.97, 0,96, 0.82, 0.6, 0.39, 0.86, 0.67]
-for file in file_list[6:7]:
+N_opt = [2,2,2,2,2,2]
+#new_Thr_opt = [8.5,8, 5.5, 10.5, 6.5, 10.5, 6.5, 6.5]
+#new_F1 = [0.97,0.96, 0.82, 0.6, 0.39, 0.86, 0.67, 0.76]
+#
+#Thr_opt = [8.5,8.5, 5.5, 10.5, 6.5, 10.5, 6.5 ]
+#F1 = [0.97, 0.96, 0.82, 0.6, 0.39, 0.86, 0.67]
+all_f1_scores = []
+all_corr_subthr = []
+
+for file in file_list[:]:
     dict1 = np.load(file, allow_pickle=True)
     img = dict1['v_sg']
+    print(np.diff( dict1['v_t']))
     std_estimate = np.diff(np.percentile(img,[75,25]))/4
     for i in range(len(dict1['sweep_time']) - 1):
         idx_to_rem = np.where([np.logical_and(dict1['v_t']>(dict1['sweep_time'][i][-1]), dict1['v_t']<dict1['sweep_time'][i+1][0])])[1]
@@ -181,14 +374,21 @@ for file in file_list[6:7]:
         idx_to_rem = np.where([np.logical_and(dict1['v_t']>(dict1['sweep_time'][i][-1]-1), dict1['v_t']<dict1['sweep_time'][i][-1]-0.85)])[1]
         img[idx_to_rem] = np.random.normal(0,1,len(idx_to_rem))*std_estimate
     
-    delta_img = np.diff(img)
-    N=2
-    indexes, erf, z_signal = find_spikes(delta_img, thres_STD=6.5, min_dist=1, N=N, win_size=20000, 
-                stride=5000, spike_before=3, spike_after=4, bidirectional=False)
+    sub_1 = estimate_subthreshold(img, thres_STD=5)
+    all_corr_subthr.append([np.corrcoef(normalize(dict1['e_sub']),normalize(sub_1))[0,1],np.corrcoef(normalize(dict1['e_sub']),normalize(dict1['v_sub']))[0,1]])
+#    delta_img = np.diff(img)
+    signal_no_subthr = dict1['v_sg'] - dict1['v_sub']     
+    indexes, erf, z_signal = find_spikes(img, signal_no_subthr=signal_no_subthr, 
+                                         thres_STD=5, thres_STD_ampl=4, min_dist=1, 
+                                         N=2, win_size=20000, stride=5000, 
+                                         spike_before=3, spike_after=4, 
+                                         bidirectional=False)
+    
     dict1_v_sp_ = dict1['v_t'][indexes]
-    range_run = estimate_running_std(delta_img, 20000, 5000, q_min=0.000001, q_max=99.999999)
-    std_run = estimate_running_std(delta_img, 20000, 5000, q_min=25, q_max=75)
-    plt.plot(range_run/std_run)
+    
+#    range_run = estimate_running_std(delta_img, 20000, 5000, q_min=0.000001, q_max=99.999999)
+#    std_run = estimate_running_std(delta_img, 20000, 5000, q_min=25, q_max=75)
+#    plt.plot(range_run/std_run)
     for i in range(len(dict1['sweep_time']) - 1):
         dict1_v_sp_ = np.delete(dict1_v_sp_, np.where([np.logical_and(dict1_v_sp_>dict1['sweep_time'][i][-1], dict1_v_sp_<dict1['sweep_time'][i+1][0])])[1])
     dict1_v_sp_ = np.delete(dict1_v_sp_, np.where([dict1_v_sp_>dict1['sweep_time'][i+1][-1]])[1])
@@ -205,7 +405,8 @@ for file in file_list[6:7]:
     
     
     
-        
+    all_f1_scores.append(np.array(F1).mean().round(2))
+    continue 
     pr.append(np.array(precision).mean().round(2))
     re.append(np.array(recall).mean().round(2))
     F.append(np.array(F1).mean().round(2))
@@ -237,114 +438,34 @@ for file in file_list[6:7]:
     ax4 = fig.add_axes([0.05, 0, 0.9, 0.15])
     ax4.plot(mean_time, sub_corr, 'o-', c='blue')
     #plt.savefig(f'{volpy_path}/metric_{vpy.params.volspike["threshold_method"]}.pdf', bbox_inches='tight')
-ax3.legend([f'precision:{pr}', f'recall: {re}', f'F1: {F}'])
-ax4.legend([f'corr:{sub}'])
+if False:    
+    ax3.legend([f'precision:{pr}', f'recall: {re}', f'F1: {F}'])
+    ax4.legend([f'corr:{sub}'])
 #%%
-
+print(np.mean(all_f1_scores))
+print(np.mean(all_corr_subthr,axis=0))
 #%%
-plt.plot(dict1['e_t'],eph)
-plt.plot(dict1['e_sp'],[1]*len(dict1['e_sp']),'.')
-plt.plot(dict1['v_t'],img)
-#%%
-plt.plot(dict1['e_sp'],[1]*len(dict1['e_sp']),'.')
-plt.plot(dict1['v_t'][1:],np.diff(img))
-#%%
-#indexes = peakutils.indexes(np.diff(img), thres=0.18, min_dist=3, thres_abs=True)
-eph = (dict1['e_sg']-np.mean(dict1['e_sg']))/(np.max(dict1['e_sg'])-np.min(dict1['e_sg']))
-#img = (dict1['v_sg']-np.mean(dict1['v_sg']))/np.max(dict1['v_sg'])
-plt.figure()
-FN = list(set(dict1_v_sp_)-set(v_match))
-FP = list(set(dict1['e_sp'])-set(e_match))
-plt.plot(dict1['e_sp'],[1.1]*len(dict1['e_sp']),'k.')
-plt.plot(dict1['v_sp'],[1.08]*len(dict1['v_sp']),'g.')
-plt.plot(FP,[1.06]*len(FP),'c|')
-plt.plot(FN,[1.04]*len(FN),'b|')
-plt.plot(dict1_v_sp_,[1.02]*len(dict1_v_sp_),'r.')
-plt.plot(dict1['v_t'][1:],np.diff(img)/np.max(np.diff(img)),'.-')
-plt.plot(dict1['e_t'],eph/np.max(eph), color='k')
-plt.plot(dict1['v_t'],img/np.max(img),'-')
-plt.plot(dict1['v_t'][1:],-erf/np.max(-erf),'r-')
-#%%
-indexes, erf, z_sig = find_spikes(delta_img, thres_STD=6, N=2)
-
-#%%
-from caiman.base.movies import rolling_window
-from functools import partial
-import cv2
-import scipy
-
-def estimate_running_std(signal_in, win_size, stride, idx_exclude=None, q_min=25, q_max=75):
-    if idx_exclude is not None:
-        signal = signal_in[np.setdiff1d(range(len(signal_in)), idx_exclude)]        
-    else:
-        signal = signal_in
-    iter_win = rolling_window(signal[None,:],win_size,stride)
-    myperc = partial(np.percentile, q=[q_min,q_max], axis=-1)
-    res = np.array(list(map(myperc,iter_win))).T.squeeze()
-    iqr = (res[1]-res[0])/1.35
-    std_run = cv2.resize(iqr,signal_in[None,:].shape).squeeze()
-    return std_run
-
-def extract_exceptional_events(z_signal, thres_STD=7.7, N=2, min_dist=1, bidirectional=False):
-    if bidirectional:        
-        erf = scipy.special.log_ndtr(-np.abs(z_signal))
-    else:
-        erf = scipy.special.log_ndtr(-z_signal)
-    erfc = np.cumsum(erf)
-    erfc[N:] -= erfc[:-N]
-    indexes = peakutils.indexes(-erfc, thres=thres_STD, min_dist=min_dist, thres_abs=True)
-    return indexes, erfc
+if False:
+    #indexes = peakutils.indexes(np.diff(img), thres=0.18, min_dist=3, thres_abs=True)
+    eph = (dict1['e_sg']-np.mean(dict1['e_sg']))/(np.max(dict1['e_sg'])-np.min(dict1['e_sg']))
+    #img = (dict1['v_sg']-np.mean(dict1['v_sg']))/np.max(dict1['v_sg'])
+    plt.figure()
+    FN = list(set(dict1_v_sp_)-set(v_match))
+    FP = list(set(dict1['e_sp'])-set(e_match))
+    plt.plot(dict1['e_sp'],[1.1]*len(dict1['e_sp']),'k.')
+    plt.plot(dict1['v_sp'],[1.08]*len(dict1['v_sp']),'g.')
+    plt.plot(FP,[1.06]*len(FP),'c|')
+    plt.plot(FN,[1.04]*len(FN),'b|')
+    plt.plot(dict1_v_sp_,[1.02]*len(dict1_v_sp_),'r.')
+    plt.plot(dict1['v_t'][1:],np.diff(img)/np.max(np.diff(img)),'.-')
+    plt.plot(dict1['e_t'],eph/np.max(eph), color='k')
+    plt.plot(dict1['v_t'],img/np.max(img),'-')
+    plt.plot(dict1['v_t'][1:],-erf/np.max(-erf),'r-')
+    plt.plot(dict1['v_t'],subs/3)
     
-#%%     
-def find_spikes(signal, thres_STD=7.7, min_dist=1, N=2, win_size=10000, 
-                stride=5000, spike_before=3, spike_after=4,
-                q_min=25, q_max=75, bidirectional=False):
-    std_run = estimate_running_std(signal, win_size, stride, q_min=q_min, q_max=q_max)
-    z_signal = signal/std_run
-    index_exceptional,_ = extract_exceptional_events(z_signal, thres_STD=thres_STD, N=N, min_dist=min_dist, bidirectional=bidirectional)    
-    index_remove = np.concatenate([index_exceptional+ii for ii in range(-spike_before,spike_after)])
-    std_run = estimate_running_std(signal, win_size, stride,idx_exclude=index_remove, q_min=q_min, q_max=q_max)
-    z_signal = delta_img/std_run 
-    indexes, erf = extract_exceptional_events(z_signal, thres_STD=thres_STD, N=N, min_dist=min_dist, bidirectional=bidirectional)
-    return indexes, erf, z_signal
 #%%
-from scipy.interpolate import interp1d
-import scipy
-
-def nan_helper(y):
-    return np.isnan(y), lambda z: z.nonzero()[0]
-
-
-def  estimate_subthreshold(signal,spike_before=3, spike_after=4):
-  delta_sig = np.diff(signal)
-  index_exceptional, erf, z_sig = find_spikes(delta_sig, thres_STD=3, N=2)
-  index_remove = np.concatenate([index_exceptional+ii for ii in range(-spike_before,spike_after)])
-  sig_sub = signal.copy()
-  sig_sub[np.minimum(index_remove,len(signal)-1)] = np.nan
-  nans, x= nan_helper(sig_sub)
-  sig_sub[nans]= np.interp(x(nans), x(~nans), sig_sub[~nans])
-  sig_sub = scipy.signal.medfilt(sig_sub, kernel_size=21)
-  return sig_sub
-  
-#%%
-dict1_v_sp_ = dict1['v_t'][indexes+1]
-FN = list(set(dict1_v_sp_)-set(v_match))
-FP = list(set(dict1['e_sp'])-set(e_match))
-plt.plot(dict1['e_sp'],[0.425]*len(dict1['e_sp']),'k.')
-plt.plot(dict1['v_sp'],[0.4]*len(dict1['v_sp']),'g.')
-dict1_v_sp_ = dict1['v_t'][indexes+1]
-#plt.plot(FP,[0.55]*len(FP),'co')
-#plt.plot(FN,[0.5]*len(FN),'bo')
-plt.plot(dict1_v_sp_,[0.45]*len(dict1_v_sp_),'r.')
-plt.plot(dict1['v_t'],img/np.max(img)/3,'.-')
-plt.plot(dict1['v_t'][1:],-erf/100)
-#plt.plot(dict1['v_t'][1:],np.diff(img,1),'.-')
-#plt.plot(dict1['e_t'],eph/3, color='c')
-#%%
-n_std=1
-new_dat = delta_img.copy()
-for i in range(10):
-    std_1 = np.diff(np.percentile(new_dat,[30.9,69.1]))/2
-    new_dat = new_dat[new_dat<(n_std*std_1)]
-    print([std_1, len(new_dat)])
-    
+if False:
+    sub_1 = estimate_subthreshold(img, thres_STD=3.5)
+    plt.plot(normalize(dict1['e_sub']),'k')
+    plt.plot(normalize(dict1['v_sub']))
+    plt.plot(normalize(sub_1))    
